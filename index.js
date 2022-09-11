8ad0f3daaa472769e7e9d4aba9203ebf99d2d544
Nicole Robbins <nicole.robbins@att.net>
2022-09-10 20:33:48 -0500

    commit tree 6527c4954c0180713c0d2a507b6964bfb874c3ee

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+    return Math.abs(42-someValue);
+}
+function distanceFromHqInFeet(someValue){
+    return (distanceFromHqInBlocks(someValue)*264);
+}
+function distanceTravelledInFeet(start, destination){
+    return Math.abs((start-destination)*264);
+}
+function calculatesFarePrice(start, destination){
+    let distance = Math.abs((start-destination)*264)
+    if (distance < 400){
+        return 0;
+    }
+    else if (distance > 2500){
+        return "cannot travel that far";
+    }
+    else if (distance > 2000){
+        return 25;
+    }
+    else if(distance >= 400){
+        return (distance-400)*.02;
+    }
+    
+}
+
